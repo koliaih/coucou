5fce94108ee5d83f1c47a7a5968e97498eefad89
hamza.koliai <hamza.koliai@etu.univ-amu.fr>
2025-05-22 16:25:23 +0200

    Initial commit

---
+++ src/main/java/fr/amu/iut/exercice17/AppMaths.java
@@ -0,0 +1,18 @@
+package fr.umontpellier.iut.exercice1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppMaths extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        throw new RuntimeException("Méthode à implémenter");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
